5e1aff04d7f916254b8781bae2c78dce3fc7807a
ZUNbado <git@acs.li>
2015-05-29 13:33:27 +0200

    inventory view

--- apps/host/urls.py
+++ apps/host/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns, include, url
 
-from .views import HostView
+from .views import HostView, InventoryView
 
 urlpatterns = patterns('',
         url(r'^host/?$', HostView.as_view()),
         url(r'^host/(?P<host_id>[0-9]+)/?$', HostView.as_view()),
+        url(r'^inventory/?$', InventoryView.as_view()),
     )
--- apps/host/views.py
+++ apps/host/views.py
@@ -10,6 +10,39 @@ from apps.variables.models import SimpleVariableValue, GroupVariableValue, Group
 
 import json
 
+def getvars_from_object(obj):
+    obj_type = ContentType.objects.get_for_model(obj)
+    variables = {}
+    for sv in SimpleVariableValue.objects.filter(host_id = obj.pk, host_type = obj_type):
+        variables[sv.variable.name] = sv.value
+    for gv in GroupVariableValue.objects.filter(host_id = obj.pk, host_type = obj_type):
+        groupvar = {}
+        for gvv in GroupVariableItemValue.objects.filter(variable = gv):
+            groupvar[gvv.variable.name] = gvv.value
+
+        variables[gv.variable.name] = groupvar
+
+    return variables
+
+
+class InventoryView(TemplateView):
+    def get(self, request, **kwargs):
+        inventory = {}
+        for hg in HostGroup.objects.all():
+            # hosts for group
+            hg_hosts = Host.objects.filter(hostgroup=hg)
+            hosts = [host.name for host in hg_hosts]
+            inventory[hg.name] = { 'hosts' : hosts, 'vars' : getvars_from_object(hg) }
+
+        meta = {}
+        for hostj in Host.objects.all():
+            meta[host.name] = getvars_from_object(host)
+        inventory['_meta'] = { 'hostvars' : meta }
+
+        return HttpResponse(json.dumps(inventory, indent=2), content_type='application/json', status=200)
+
+
+
 class HostView(TemplateView):
     def get(self, request, host_id=None, **kwargs):
         if host_id:
