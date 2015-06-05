d1435c44e0407502c6cc8ada76cc1f0a646f038e
ZUNbado <git@acs.li>
2015-06-05 12:10:57 +0200

    changes in inventory

--- apps/host/views.py
+++ apps/host/views.py
@@ -15,22 +15,23 @@ def getvars_from_object(obj):
     variables = {}
     for sv in SimpleVariableValue.objects.filter(host_id = obj.pk, host_type = obj_type):
         if sv.variable.name in variables:
-            if type(variables[sv.variable.name]) != list():
+            if type(variables[sv.variable.name]) != list:
                 variables[sv.variable.name] = [ variables[sv.variable.name] ]
             variables[sv.variable.name].append(sv.value)
         else:
             variables[sv.variable.name] = sv.value
     for gv in GroupVariableValue.objects.filter(host_id = obj.pk, host_type = obj_type):
         groupvar = {}
-        for gvv in GroupVariableItemValue.objects.filter(variable = gv):
-            if gvv.variable.name in groupvar:
-                if type(groupvar[gvv.variable.name]) != list():
-                    groupvar[gvv.variable.name] = [ groupvar[gvv.variable.name] ]
-                groupvar[gvv.variable.name].append(gvv.value)
-            else:
+        for gvv in GroupVariableItemValue.objects.filter(group = gv):
+            if gvv.value:
                 groupvar[gvv.variable.name] = gvv.value
 
-        variables[gv.variable.name] = groupvar
+        if gv.variable.name in variables:
+            if type(variables[gv.variable.name]) != list:
+                variables[gv.variable.name] = [ variables[gv.variable.name] ]
+            variables[gv.variable.name].append(groupvar)
+        else:
+            variables[gv.variable.name] = groupvar
 
     return variables
 
